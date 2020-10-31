7290d2a599bf435c418af117de3b5b6b6da533df
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-10-31 17:05:51 +0900

    Create message.hook.js

---
+++ client/src/hooks/message.hook.js
@@ -0,0 +1,10 @@
+import { useCallback } from "react";
+
+// materialize function
+export const useMessage = () => {
+  return useCallback((text) => {
+    if (window.M && text) {
+      window.M.toast({ html: text });
+    }
+  }, []);
+};
