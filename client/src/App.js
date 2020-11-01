c55cf8a902b304c7dda2d5a4ac98e17c3adce3dd
Alexander BORISOV <67188470+syzygyexe@users.noreply.github.com>
2020-11-01 22:18:40 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom";
 import { useRoutes } from "./routes";
+import { useAuth } from "./hooks/auth.hook";
+import { AuthContext } from "./context/AuthContext";
+import { Navbar } from "./Components/Navbar";
 import "materialize-css";
 
 function App() {
-    const routes = useRoutes(false)
-    return (
-        <Router>
-            <div className="container">
-                {routes}
-            </div>
-        </Router>
-    )
+  const { token, login, logout, userId } = useAuth();
+  // Checking whether user is authenticated or not, by providing token. !! is a boolean transformation.
+  // Our token can take only two values true or false, and based on that we will make further render.
+  const isAuthenticated = !!token;
+  const routes = useRoutes(isAuthenticated);
+  return (
+    <AuthContext.Provider
+      value={{ token, login, logout, userId, isAuthenticated }}
+    >
+      <Router>
+        {/* Render Navbar when authenticated */}
+        {isAuthenticated && <Navbar />}
+        <div className='container'>{routes}</div>
+      </Router>
+    </AuthContext.Provider>
+  );
 }
 
-export default App
+export default App;
